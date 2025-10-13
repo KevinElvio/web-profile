aa149d7981f88640e5bfb50c5c554b9b7956bcf3
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-10-13 22:47:30 +0700

    FIX: page

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,17 @@
-import LandingPage from './pages/landingPage.jsx'
-import PageAdmin from './pages/admin/appAdmin.jsx'
-import Login from './pages/auth/login.jsx';
-import ProjectAdmin from './pages/admin/projectAdmin.jsx';
+// import LandingPage from './pages/landingPage.jsx'
+// import PageAdmin from './pages/admin/appAdmin.jsx'
+// import Login from './pages/auth/login.jsx';
+// import ProjectAdmin from './pages/admin/projectAdmin.jsx';
+import Portfolio from './pages/landingPage.jsx';
 import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<LandingPage />} />
-      <Route path='/admin' element={<PageAdmin />} />
+      <Route path='/' element={<Portfolio />} />
+      {/* <Route path='/admin' element={<PageAdmin />} />
       <Route path='/login' element={<Login />} />
-      <Route path='/projects' element={<ProjectAdmin />} />
+      <Route path='/projects' element={<ProjectAdmin />} /> */}
     </Routes>
   );
 }
