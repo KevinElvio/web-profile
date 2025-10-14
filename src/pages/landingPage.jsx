5ec8b540b4af590e654bf1eeac55afe89dbd1d48
Kevin Elvio <62099198+KevinElvio@users.noreply.github.com>
2025-10-15 06:10:27 +0700

    Fix

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,30 +1,3 @@
-// import About from "./layouts/About";
-// import Biodata from "./layouts/Biodata";
-// import Experience from "./layouts/Experience";
-// import Footer from "../components/Footer";
-// import Navbar from "../components/Navbar";
-// import Project from "./layouts/Project";
-// import Skills from "./layouts/Skills";
-
-// import { StrictMode } from 'react';
-
-// function LandingPage() {
-//     return (
-//         <StrictMode>
-//             <Navbar />
-//             <Biodata />
-//             <About />
-//             <Project />
-//             <Skills />
-//             <Experience />
-//             <Footer />
-//         </StrictMode>
-//     );
-// }
-
-// export default LandingPage;
-
-
 import { useState, useEffect, useRef } from 'react';
 import { motion, useInView, AnimatePresence } from 'framer-motion';
 
